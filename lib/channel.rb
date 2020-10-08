b6bd809fa194c472536cfb879cbfdabdb726d30a
Genevieve Neely <genevieve.hood@gmail.com>
2020-10-08 14:40:35 -0700

    implemented Chanel#load_all + tests

--- lib/channel.rb
+++ lib/channel.rb
@@ -4,6 +4,8 @@ require_relative 'builder'
 
 class Channel < Builder
 
+  SLACK_URL = "https://slack.com/api/conversations.list"
+
   attr_reader :topic, :member_count
 
   def initialize(id:, name:, topic:, member_count:)
@@ -12,5 +14,23 @@ class Channel < Builder
     @topic = topic
     @member_count = member_count
 
+  end
+
+  def details
+    return "ID: #{id}\nName: #{name}\nTopic:#{topic}\n# Members #{member_count}"
+  end
+
+  def self.list_all
+    parameters = {token: ENV["SLACK_TOKEN"]}
+    response = self.get(SLACK_URL, parameters)
+
+    response["channels"].map do |channel|
+      new(
+          id: channel["id"],
+          name: channel["name"],
+          topic: channel["topic"]["value"],
+          member_count: channel["num_members"]
+      )
+    end
   end
 end
--- test/channel_test.rb
+++ test/channel_test.rb
@@ -1,14 +1,16 @@
 
-
+require_relative 'test_helper'
 
 describe "Channel class" do
 
+  before do
+    @new_channel = Channel.new( id: "1234",
+                                name: "schanen",
+                                topic: "general",
+                                member_count: 456)
+  end
+
   describe "initialize" do
-
-    before do
-      @new_channel = Channel.new( id: 1234, name: "schanen", topic: "general", member_count: 456)
-    end
-
     it "is an instance of Channel" do
       expect(@new_channel).must_be_instance_of Channel
     end
@@ -17,5 +19,31 @@ describe "Channel class" do
       expect(@new_channel).must_be_kind_of Builder
     end
 
+    it "returns the correct instance variable values" do
+      expect(@new_channel.id).must_equal "1234"
+      expect(@new_channel.name).must_equal "schanen"
+      expect(@new_channel.topic).must_equal "general"
+      expect(@new_channel.member_count).must_equal 456
+    end
+  end
+
+  describe "details" do
+    it "returns a string of channel information" do
+      channel_details = @new_channel.details
+      expect(channel_details).must_be_instance_of String
+    end
+  end
+
+  describe "list_all" do
+    it "returns an array of Channel objects" do
+      VCR.use_cassette("channel_list_all") do
+        channel_array = Channel.list_all
+
+        expect(channel_array).must_be_instance_of Array
+        channel_array.each do |channel|
+          _(channel).must_be_instance_of Channel
+        end
+      end
+    end
   end
 end
