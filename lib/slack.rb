00310341b5ac9ae1807273d4f3076198caef1236
Schanen Ryan <schanen.r@gmail.com>
2020-10-08 16:33:47 -0700

    CLI printing draft 1

--- lib/slack.rb
+++ lib/slack.rb
@@ -6,11 +6,29 @@ def main
   puts "Welcome to the Ada Slack CLI!"
   workspace = Workspace.new
 
-  # TODO project
+  list_options = user_options
+
+  until list_options == "quit"
+    if list_options == "list users"
+      puts workspace.users
+      list_options = user_options
+    elsif list_options == "list channels"
+      puts workspace.channels
+      list_options = user_options
+    else
+      list_options = user_options
+    end
+  end
 
   puts "Thank you for using the Ada Slack CLI"
 end
 
-main if __FILE__ == $PROGRAM_NAME
+def user_options
+  puts "Which action would you like to take in our Slack Workspace?"
+  puts "list users"
+  puts "list channels"
+  puts "quit"
+  return gets.chomp.downcase
+end
 
-puts main
+main if __FILE__ == $PROGRAM_NAME
